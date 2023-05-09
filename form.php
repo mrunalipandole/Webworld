4574cf0fd288e8dbbb7ce7d0f46a1741c200a36b
mrunalipandole <130342933+mrunalipandole@users.noreply.github.com>
2023-05-09 13:14:19 +0530

    Add files via upload

---
+++ form.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"    >
+   
+
+    <title>Document</title>
+</head>
+<body>
+<form action="abc.php" method="POST" oninput="sum.value=parseInt(x.value) + parseInt(y.value)">
+        Name:
+        <input type="text" name="fname" placeholder="First Name" required>
+        <br>
+        Surname:
+        <input type="text" name="sname" placeholder="Surname" required>
+        Mobile:
+        <input type="number" name="Contact" placeholder="Contact" required>
+     
+       
+        <br>
+        Email:
+        <input type="email" name="email" placeholder="Email" required>
+     
+       
+        <br>
+       
+        <br>
+       
+        <input type="submit">
+  
+
+    </form>
+</body>
+</html>
